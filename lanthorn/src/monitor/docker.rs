9a08cdc1b9c395f99f9bf68cfa79311e504f6c2e
Oleh Zinko <renderhp@gmail.com>
2025-12-14 16:44:20 +0000

    refactor(dockermon): get rid of unwraps inside docker.rs, add error messages

--- lanthorn/src/monitor/docker.rs
+++ lanthorn/src/monitor/docker.rs
@@ -1,12 +1,12 @@
-use bollard::{Docker, query_parameters::ListContainersOptions};
-use log::info;
+use bollard::{Docker, query_parameters::ListContainersOptions, secret::ContainerSummary};
+use log::{error, info};
 use std::{collections::HashMap, os::unix::fs::MetadataExt};
 
 #[derive(Debug)]
 struct MonitoredContainer {
     id: String,
-    names: Vec<String>,
-    image: String,
+    names: Option<Vec<String>>,
+    image: Option<String>,
     pid: i64,
     cgroup_id: u64,
 }
@@ -25,37 +25,76 @@ pub async fn run_docker_monitor() -> Result<(), anyhow::Error> {
     let mut docker_state: HashMap<String, MonitoredContainer> = HashMap::new();
 
     for container in running_containers {
-        let pid = docker
-            .inspect_container(
-                container.id.clone().unwrap().as_str(),
-                None::<bollard::query_parameters::InspectContainerOptions>,
+        if let Some(item) = get_monitored_container(&docker, &container).await {
+            docker_state.insert(item.id.clone(), item);
+        } else {
+            error!(
+                "Failed to insert container with ID {:?} to cache",
+                &container.id,
             )
-            .await
-            .unwrap()
-            .state
-            .and_then(|v| v.pid)
-            .unwrap();
-
-        let cgroup_path = std::fs::read_to_string(format!("/proc/{}/cgroup", pid))?;
-        let cgroup_path = cgroup_path
-            .lines()
-            .next()
-            .and_then(|line| line.split("::").nth(1))
-            .unwrap();
-        let cgroup_path = format!("/sys/fs/cgroup/{}", cgroup_path);
-        let cgroup_id = std::fs::metadata(&cgroup_path)?.ino();
-
-        let item = MonitoredContainer {
-            id: container.id.clone().unwrap(),
-            names: container.names.unwrap(),
-            image: container.image.unwrap(),
-            pid,
-            cgroup_id,
-        };
-        docker_state.insert(container.id.unwrap(), item);
+        }
     }
 
     println!("Existing Containers:\n{:#?}", docker_state);
-
     Ok(())
+}
+
+async fn get_monitored_container(
+    docker: &Docker,
+    container: &ContainerSummary,
+) -> Option<MonitoredContainer> {
+    let Some(id) = container.id.clone() else {
+        error!("ID missing from container {:?}. Skipping...", &container);
+        return None;
+    };
+
+    let info = docker
+        .inspect_container(
+            &id,
+            None::<bollard::query_parameters::InspectContainerOptions>,
+        )
+        .await
+        .inspect_err(|e| error!("Failed to inspect container {}: {}", id, e))
+        .ok()?;
+
+    let pid = info.state.and_then(|v| v.pid).or_else(|| {
+        error!("Couldn't get pid for container with id: {}", &id);
+        None
+    })?;
+
+    let cgroup_path = std::fs::read_to_string(format!("/proc/{}/cgroup", pid))
+        .inspect_err(|e| {
+            error!(
+                "Failed to get cgroup path for container with ID {}: {}",
+                &id, e
+            )
+        })
+        .ok()?;
+
+    let cgroup_path = cgroup_path
+        .lines()
+        .next()
+        .and_then(|line| line.split("::").nth(1))
+        .or_else(|| {
+            error!("Failed to parse cgroup path from /proc/{}/cgroup", pid);
+            None
+        })?;
+    let cgroup_path = format!("/sys/fs/cgroup/{}", cgroup_path);
+    let cgroup_id = std::fs::metadata(&cgroup_path)
+        .inspect_err(|e| {
+            error!(
+                "Failed to fetch metadata for container with ID {}: {}",
+                &id, e
+            )
+        })
+        .ok()?
+        .ino();
+
+    Some(MonitoredContainer {
+        id,
+        names: container.names.clone(),
+        image: container.image.clone(),
+        pid,
+        cgroup_id,
+    })
 }
