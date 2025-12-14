839230ab16685fa889f17ba25a11b0bc78f87158
Oleh Zinko <renderhp@gmail.com>
2025-12-14 15:34:38 +0000

    feat(dockermon): basic bollard setup

--- lanthorn/src/monitor/docker.rs
+++ lanthorn/src/monitor/docker.rs
@@ -1,6 +1,18 @@
+use bollard::{Docker, query_parameters::ListContainersOptions};
 use log::info;
+use std::collections::HashMap;
 
 pub async fn run_docker_monitor() -> Result<(), anyhow::Error> {
     info!("Starting Docker monitor...");
+    let docker = Docker::connect_with_socket_defaults()?;
+    let options = Some(ListContainersOptions {
+        all: true,
+        limit: None,
+        size: true,
+        filters: Some(HashMap::from([("status".into(), vec!["running".into()])])),
+    });
+    let containers = docker.list_containers(options).await?;
+    println!("{:?}", containers);
+
     Ok(())
 }
