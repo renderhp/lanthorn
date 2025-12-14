0c82c0182e0375028a2c2db51f36d633eba24347
Oleh Zinko <renderhp@gmail.com>
2025-12-14 14:26:01 +0000

    feat: add disable_tcp argument

--- lanthorn/src/main.rs
+++ lanthorn/src/main.rs
@@ -7,17 +7,23 @@ mod utils;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
-struct Args {}
+struct Args {
+    /// Disable TCP connection tracking through eBPF kprobe on tcp_connect
+    #[arg(long, default_value_t = false)]
+    disable_tcp: bool,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    let _args = Args::parse();
+    let args = Args::parse();
     env_logger::init();
-
     info!("Starting initialisation");
 
     storage::init().await?;
-    monitor::run_monitor().await?;
+
+    if !args.disable_tcp {
+        monitor::run_monitor().await?;
+    }
 
     info!("All components initialised. Press Ctrl+C to exit.");
 
